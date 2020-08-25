883272499bcc552b242571a0916e01b555e56a60
Matt1-Mac <matthewmrph40@gmail.com>
2020-08-25 11:51:34 -0400

    list 1 complete

--- list1.py
+++ list1.py
@@ -5,7 +5,7 @@ Kenzie assignment: List1
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Matthew Murphy"
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
@@ -27,7 +27,12 @@ __author__ = "???"
 
 def match_ends(words):
     # your code here
-    return
+    count = 0
+
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            count += 1
+    return count
 
 
 # B. front_x
@@ -43,7 +48,10 @@ def match_ends(words):
 
 def front_x(words):
     # your code here
-    return
+    x_words = [i for i in words if i[0].lower() == "x"]
+    words = [i for i in words if i[0].lower() != "x"]
+    my_list = sorted(x_words)+sorted(words)
+    return my_list
 
 
 # C. sort_last
@@ -57,7 +65,9 @@ def front_x(words):
 
 def sort_last(tuples):
     # your code here
-    return
+    def last(n): return n[-1]
+
+    return sorted(tuples, key=last)
 
 
 # Provided simple test() function used in main() to print
